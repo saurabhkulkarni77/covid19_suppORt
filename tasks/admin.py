b1bbda13238aa9e5447e6521198e4398a3c7ef2c
saurabhkulkarni77 <sskkulk@gmail.com>
2020-04-13 16:37:56 -0400

    demo

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 admin.site.register(Task)
-admin.site.register(Taskone)
+@admin.register(Taskone)
+class ViewTask(ImportExportModelAdmin):
+	pass
