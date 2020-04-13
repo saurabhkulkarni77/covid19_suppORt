0aa37b8bf3e166e56f2f413518db01a1067b113a
saurabhkulkarni77 <sskkulk@gmail.com>
2020-04-13 16:42:32 -0400

    demo

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 admin.site.register(Task)
+@admin.register(Taskone)
+class ViewTask(ImportExportModelAdmin):
+	pass
--- tasks/models.py
+++ tasks/models.py
@@ -9,3 +9,11 @@ class Task(models.Model):
 	delivery_time = models.CharField(default='take your time',max_length=1000)
 	Dietary_Restrictions = models.CharField(max_length=1000,default='As per menu')
 	created = models.DateTimeField(auto_now_add=True)
+class Taskone(models.Model):
+	your_name = models.CharField(max_length=1000)
+	email     = models.EmailField(default='forgot to enter email')
+	phone_number = models.CharField(max_length=12,default='forgot to enter phonr number')
+	order_size = models.CharField(default='forgot to enter order size',max_length=1000)
+	delivery_time = models.CharField(default='take your time',max_length=1000)
+	Dietary_Restrictions = models.CharField(max_length=1000,default='As per menu')
+	created = models.DateTimeField(auto_now_add=True)
