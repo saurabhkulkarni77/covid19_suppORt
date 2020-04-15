f90986c7bbb79389c9b26240114ad594209a17a0
saurabhkulkarni77 <sskkulk@gmail.com>
2020-04-15 12:31:42 -0400

    cosmetic changes

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,19 +1,28 @@
 from django import forms
 from .models import *
 class NameForm(forms.ModelForm):
-    your_name =forms.CharField(label='Your name', max_length=1000)
+    your_name =forms.CharField(label='Your Name', max_length=1000)
     email     = forms.EmailField(label="Enter Email")
     phone_number = forms.CharField(max_length=12)
-    order_size = forms.CharField(label='Number of people(order size)', max_length=1000)
-    delivery_time = forms.CharField(label = 'delivery time', max_length=1000)
+    order_size = forms.CharField(label='Number of People (Order Size)', max_length=1000)
+    time_choice = (
+    	('Please Select Time Slot','Please Select Time Slot'),
+    	('12:00 PM till 12:30PM','12:00 PM till 12:30PM'),
+    	('12:30 PM till 1:00PM','1:00 PM till 1:30PM'),
+    	('1:30 PM till 2:00PM','1:30 PM till 2:00PM'),
+    	('2:00 PM till 2:30PM','2:00 PM till 2:30PM'),
+    	('2:30 PM till 3:00PM','2:30 PM till 3:00PM'),
+    	('3:00 PM till 3:30PM','3:00 PM till 3:30PM'),
+    	('3:30 PM till 4:00PM','3:30 PM till 4:00PM'),
+    	('4:00 PM till 4:30PM','4:00 PM till 4:30PM'),
+    	('4:30 PM till 5:00PM','4:30 PM till 5:00PM'),
+    	('5:00 PM till 5:30PM','5:00 PM till 5:30PM'),
+    	('5:30 PM till 6:00PM','5:30 PM till 6:00PM'),
+    	('6:00 PM till 6:30PM','6:00 PM till 6:30PM'),
+    	('6:30 PM till 7:00PM','6:30 PM till 7:00PM'),
+    	)
+    delivery_time = forms.ChoiceField(widget = forms.Select(),label = 'Pickup Time',initial='Please Select Timeslot',choices=time_choice)
     Dietary_Restrictions = forms.CharField(label='Dietary Restrictions', max_length=1000)
     class Meta:
     	model = Task
     	fields = ('your_name','email','phone_number','order_size','delivery_time','Dietary_Restrictions')
-
-
-# class KitchenForm(forms.ModelForm):
-# 	complete = models.BooleanField(default=False)
-# 	class Meta:
-# 		model = Task
-# 		fields = ('complete',)
--- tasks/models.py
+++ tasks/models.py
@@ -6,7 +6,23 @@ class Task(models.Model):
 	email     = models.EmailField(default='forgot to enter email')
 	phone_number = models.CharField(max_length=12,default='forgot to enter phonr number')
 	order_size = models.CharField(default='forgot to enter order size',max_length=1000)
-	delivery_time = models.CharField(default='take your time',max_length=1000)
+	time_choice = (
+    	('Please Select Time Slot','Please Select Time Slot'),
+    	('12:00 PM till 12:30PM','12:00 PM till 12:30PM'),
+    	('12:30 PM till 1:00PM','1:00 PM till 1:30PM'),
+    	('1:30 PM till 2:00PM','1:30 PM till 2:00PM'),
+    	('2:00 PM till 2:30PM','2:00 PM till 2:30PM'),
+    	('2:30 PM till 3:00PM','2:30 PM till 3:00PM'),
+    	('3:00 PM till 3:30PM','3:00 PM till 3:30PM'),
+    	('3:30 PM till 4:00PM','3:30 PM till 4:00PM'),
+    	('4:00 PM till 4:30PM','4:00 PM till 4:30PM'),
+    	('4:30 PM till 5:00PM','4:30 PM till 5:00PM'),
+    	('5:00 PM till 5:30PM','5:00 PM till 5:30PM'),
+    	('5:30 PM till 6:00PM','5:30 PM till 6:00PM'),
+    	('6:00 PM till 6:30PM','6:00 PM till 6:30PM'),
+    	('6:30 PM till 7:00PM','6:30 PM till 7:00PM'),
+    	)
+	delivery_time = models.CharField(default='take your time',max_length=1000,choices=time_choice)
 	Dietary_Restrictions = models.CharField(max_length=1000,default='As per menu')
 	created = models.DateTimeField(auto_now_add=True)
 class Taskone(models.Model):
@@ -14,6 +30,22 @@ class Taskone(models.Model):
 	email     = models.EmailField(default='forgot to enter email')
 	phone_number = models.CharField(max_length=12,default='forgot to enter phonr number')
 	order_size = models.CharField(default='forgot to enter order size',max_length=1000)
-	delivery_time = models.CharField(default='take your time',max_length=1000)
+	time_choice = (
+    	('Please Select Time Slot','Please Select Time Slot'),
+    	('12:00 PM till 12:30PM','12:00 PM till 12:30PM'),
+    	('12:30 PM till 1:00PM','1:00 PM till 1:30PM'),
+    	('1:30 PM till 2:00PM','1:30 PM till 2:00PM'),
+    	('2:00 PM till 2:30PM','2:00 PM till 2:30PM'),
+    	('2:30 PM till 3:00PM','2:30 PM till 3:00PM'),
+    	('3:00 PM till 3:30PM','3:00 PM till 3:30PM'),
+    	('3:30 PM till 4:00PM','3:30 PM till 4:00PM'),
+    	('4:00 PM till 4:30PM','4:00 PM till 4:30PM'),
+    	('4:30 PM till 5:00PM','4:30 PM till 5:00PM'),
+    	('5:00 PM till 5:30PM','5:00 PM till 5:30PM'),
+    	('5:30 PM till 6:00PM','5:30 PM till 6:00PM'),
+    	('6:00 PM till 6:30PM','6:00 PM till 6:30PM'),
+    	('6:30 PM till 7:00PM','6:30 PM till 7:00PM'),
+    	)
+	delivery_time = models.CharField(default='take your time',max_length=1000,choices=time_choice)
 	Dietary_Restrictions = models.CharField(max_length=1000,default='As per menu')
 	created = models.DateTimeField(auto_now_add=True)
