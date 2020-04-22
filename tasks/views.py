16e4949d74832ec5533e6bc8b485486ebdc6bdf6
saurabhkulkarni77 <sskkulk@gmail.com>
2020-04-21 23:04:19 -0400

    mail removed

--- tasks/views.py
+++ tasks/views.py
@@ -81,19 +81,8 @@ def index(request):
 def deleteTask(request,pk):
 	item = Task.objects.get(id=pk)
 	if request.method =='POST':
-		to = item.email
-		customer_name = item.your_name 
 		Taskone.objects.create(your_name = item.your_name,email=item.email,phone_number=item.phone_number,order_size=item.order_size,delivery_time=item.delivery_time,Dietary_Restrictions=item.Dietary_Restrictions,Delivery_Address=item.Delivery_Address,order_choice=item.order_choice,created=item.created)
 		item.delete()
-		subject = '<DO NOT REPLY>: Kabab & Curry Order Ready'
-		context = {
-			'contact_name': customer_name}
-		email_from = settings.EMAIL_HOST_USER
-		recipient_list = [to]
-		template = get_template('kitchen.txt')
-		content = template.render(context)
-		email = EmailMessage(subject, content, email_from, recipient_list)
-		email.send()
 		return redirect('kitchen')
 	context={'item':item}
 	return render(request,'tasks/delete.html',context)
